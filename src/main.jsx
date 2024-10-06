fe6f5a7d5872561017a2cf18d4ec38c73e8dffdb
Remon321 <remon_ehab@hotmail.com>
2024-10-06 17:04:59 +0300

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
     >
       <App />
       <Toaster visibleToasts={1} duration={3000} position="top-right" richColors />
+      <Analytics />
       <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
