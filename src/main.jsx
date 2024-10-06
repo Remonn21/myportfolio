e2b105e851f46ce291701cdbcf46a080c3243878
Remon321 <remon_ehab@hotmail.com>
2024-10-06 16:43:56 +0300

    Vercel Speed Insights setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/myportfolio">
       <App />
       <Toaster visibleToasts={1} duration={3000} position="top-right" richColors />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
